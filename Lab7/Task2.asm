1d7d78f4718708719920b4534a5461ecaba443d0
MSanan791 <sanan791@gmail.com>
2024-10-28 00:34:23 +0500

    Lab Task2

--- Lab7/Task2.asm
+++ Lab7/Task2.asm
@@ -4,49 +4,92 @@ org 100h
 msg1 DB "Enter First Word",0AH, 0DH, "$"  
 newline DB "", 0AH, 0DH, "$"                                      
 msg2 DB "Enter Second Word",0AH, 0DH, "$"
-str1 DB  12 ,11 DUP(0)
-str2 DB  12 ,11 DUP(0)
+msg3 DB "STRING 1 DOES NOT PRECEDE STRING 2", 0ah, 0dh, "$" 
+msg4 DB "STRING 1 DOES PRECEED STRING 2", 0ah, 0dh, "$"
 
-
-
+str1 DB  12, 11 DUP(0)  ; Buffer for first string
+str2 DB  12, 11 DUP(0)  ; Buffer for second string
 
 .CODE
 
 MOV AX, @DATA
 MOV DS, AX  
 
+; Display first message
 LEA DX, msg1 
-MOV AH, 09H ; show first message
+MOV AH, 09H 
 INT 21H
 
+; Read first string
 MOV AH, 0AH
-MOV DX, OFFSET str1 ;INPUT VALUE
+MOV DX, OFFSET str1
 INT 21h
 
-LEA DX, newline ; NEWLINE
+; New line
+LEA DX, newline
 MOV AH, 09H
 INT 21H
  
-LEA DX, msg2 ; SHOW SECOND MESSSAGE
+; Display second message
+LEA DX, msg2
 MOV AH, 09H
 INT 21H
 
-
+; Read second string
 MOV AH, 0AH
-MOV DX, OFFSET str2 ; INPUT SECOND STRING
+MOV DX, OFFSET str2
 INT 21h
 
-; APPLYING FIRST CONDITION
-;CHECKING FOR equality in length
+; New line
+LEA DX, newline
+MOV AH, 09H
+INT 21H
+                                       
+; Check if strings have the same length
+MOV AL, str1[1]  ; Length of str1
+MOV BL, str2[1]  ; Length of str2
+CMP AL, BL
+JZ EQUAL         ; Jump if lengths are equal
 
+; If lengths are not equal
+MOV AH, 09H
+LEA DX, msg3
+INT 21H
+JMP END
 
+EQUAL: 
+MOV CX, 0
+MOV CL, str1[1]  ; Set CX to the length of the strings
+MOV SI, OFFSET str1
+ADD SI, 2        ; Point to the first character of str1
+MOV DI, OFFSET str2
+ADD DI, 2        ; Point to the first character of str2
+        
+iter:        
+    MOV AL, [SI]  ; Load character from str1
+    MOV BL, [DI]  ; Load character from str2
 
+    INC SI
+    INC DI
 
+    CMP AL, BL
+    JB preceed     ; If str1 < str2, jump to preceed
 
+    CMP AL, BL
+    JA normal      ; If str1 > str2, jump to normal
 
+    LOOP iter      ; Continue looping if CX != 0
 
-ret
+normal:
+    LEA DX, msg3   ; Load msg3 if str1 does not precede str2
+    MOV AH, 09H
+    INT 21H      
+    JMP END
 
+preceed:
+    LEA DX, msg4   ; Load msg4 if str1 precedes str2
+    MOV AH, 09H
+    INT 21H
 
-
-
+END:
+    ret
